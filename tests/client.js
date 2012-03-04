86b79ca43358e5626e375343e0da502773a11d91
Carlos Guerreiro <carlos@perceptiveconstructs.com>
2012-03-04 18:13:33 +0200

    quitting as soon as both operations are completed

--- tests/client.js
+++ tests/client.js
@@ -9,13 +9,22 @@ var recurrent = require('../recurrent.js');
 
 var c = new recurrent.Client('q');
 
+var remaining = 2;
 c.add('t1', new Date().getTime() + 30000, function(err, results) {
   if(err)
     throw err;
   console.log(results);
+  --remaining;
+  if (remaining === 0) {
+    c.quit();
+  }
 });
 c.add('t2', new Date().getTime() + 15000, function(err, results) {
   if(err)
     throw err;
   console.log(results);
+  --remaining;
+  if (remaining === 0) {
+    c.quit();
+  }
 });
