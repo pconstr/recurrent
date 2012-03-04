c8ebd878e5e034b1b04aba0c834c51e0104ecff9
Carlos Guerreiro <carlos@perceptiveconstructs.com>
2012-03-04 17:33:11 +0200

    test recurrent manager

---
+++ manager1.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+
+/* Copyright 2011 Carlos Guerreiro
+   All rights reserved */
+
+'use strict';
+
+var recurrent = require('./recurrent.js');
+
+var m = new recurrent.Manager('q', 6789);
+
+m.go();
