9d13c625c65f13f5247e528bd8b9ce901056baed
Carlos Guerreiro <carlos@perceptiveconstructs.com>
2012-03-04 23:31:52 +0200

    handling failure. quitting after adding tasks

--- tests/client.js
+++ tests/client.js
@@ -12,19 +12,23 @@ var c = new recurrent.Client('q');
 var remaining = 2;
 c.add('t1', new Date().getTime() + 30000, function(err, results) {
   if(err)
-    throw err;
-  console.log(results);
+    console.error('could not add');
+  else
+    console.error(results);
   --remaining;
   if (remaining === 0) {
+    console.error('quitting');
     c.quit();
   }
 });
 c.add('t2', new Date().getTime() + 15000, function(err, results) {
   if(err)
-    throw err;
-  console.log(results);
+    console.error('could not add');
+  else
+    console.error(results);
   --remaining;
   if (remaining === 0) {
+    console.error('quitting');
     c.quit();
   }
 });
